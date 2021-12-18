88daa1e05dfd590deb00ddf9a7fabdcabdafdb66
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-18 12:18:22 +0900

    apply style

--- src/content-script.js
+++ src/content-script.js
@@ -1,6 +1,16 @@
 function main() {
     const [body] = document.getElementsByTagName('body');
     const div = document.createElement('div');
+    div.setAttribute(
+        'style',
+        `
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        background: #0000004d;
+        `
+    );
     body.appendChild(div);
 }
 
